f2f460af3ff45188159a46bdf7a0356dd9c1daf3
nekojanai <ohayou@cock.li>
2020-11-18 00:50:59 +0100

    T-T

--- apps/nobs/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  API_BASE_URL: 'http://localhost:3333/api/v1/',
-  WS_BASE_URL: 'http://localhost:3333',
-  S3_BASE_URL: 'http://localhost:9000',
-  LIVE_URL: 'http://localhost'
-};
