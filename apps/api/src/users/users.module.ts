5eed42299d2decfa703ea1c2d01dbc6af57cc8ae
nekojanai <ohayou@cock.li>
2020-11-18 00:53:25 +0100

    uwaa

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -3,18 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { User } from './user.entity';
 import { PublicUsersController } from './public-users.controller';
 import { ActivityPubService } from './activity-pub.service';
-import { ConfigModule } from '@nestjs/config';
 import { RemoteInstance } from './remote-instance.entity';
 import { RemoteActor } from './remote-actor.entity';
 
 @Module({
 	imports: [
 		TypeOrmModule.forFeature([UserRepository,RemoteInstance,RemoteActor]),
-		HttpModule,
-		ConfigModule
+		HttpModule
 	],
 	exports: [UsersService],
 	controllers: [UsersController, PublicUsersController],
--- apps/api/src/webfinger/webfinger.module.ts
+++ apps/api/src/webfinger/webfinger.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebfingerService } from './webfinger.service';
 import { WebfingerController } from './webfinger.controller';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
-    ConfigModule,
     UsersModule
   ],
   providers: [WebfingerService],
